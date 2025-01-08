a931e2aec03e7eac7251413eb4c59694e21e1054
Adene Daniel <philocity@proton.me>
2025-01-08 04:58:50 -0800

    Update JS-DSA.ts

--- JS-DSA.ts
+++ JS-DSA.ts
@@ -1,1 +1,3 @@
-let character;
+let character = "I";  // Initializing a variable //
+console.log(charcter);  // printing out a value onto the console, useful for debugging //
+character = "Him within";  // Reassigning a variable //
