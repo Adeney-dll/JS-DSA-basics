ec4552e331bc1176aad764b4e2003ffc8e60ac71
Adene Daniel <philocity@proton.me>
2025-01-08 09:03:57 -0800

    Update JS-DSA.js

--- JS-DSA.js
+++ JS-DSA.js
@@ -1,3 +1,4 @@
+// New javascript notes from freecodecamp js and dsa course //
 let character = "I";  // Initializing a variable //
 console.log(charcter);  // printing out a value onto the console, useful for debugging //
 character = "Him within";  // Reassigning a variable //
