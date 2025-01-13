60642a3e048b343fc6be21e991809edcdc2cedfb
Adene Daniel <philocity@proton.me>
2025-01-13 07:30:41 -0800

    Update JS-DSA.js

--- JS-DSA.js
+++ JS-DSA.js
@@ -3,6 +3,5 @@ let character = "I";  // Initializing a variable //
 console.log(charcter);  // printing out a value onto the console, useful for debugging //
 character = "Him within";  // Reassigning a variable //
 let theNewOne  // camelcase writing for js //
-let theNewOne  // camelcase writing for js //
 let row = ["john", "Joe", "Jake", "Jonathan"]
 row[1] = "Jane"
