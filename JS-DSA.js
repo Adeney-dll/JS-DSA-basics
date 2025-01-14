67fa76f78f1614c422467da3e48f60c295e8160a
Adene Daniel <philocity@proton.me>
2025-01-14 13:08:28 +0100

    Update JS-DSA.js

--- JS-DSA.js
+++ JS-DSA.js
@@ -5,3 +5,22 @@ character = "Him within";  // Reassigning a variable //
 let theNewOne  // camelcase writing for js //
 let row = ["john", "Joe", "Jake", "Jonathan"];  // arrays //
 row[1] = "Jane";  // changing values in an array //
+
+ let animal = ["dolphin", "bear", "seagull", "camel", "shark"];  //arrays 
+ let coolStuff = [["Ships", "Stars", "Jump"], ["Girls", "Guns", "SMGS"], "Querty"];  // Nested arrays, an array within an array
+
+
+                let randoNumbero = [20, 50, 37, 22, 90, 30, 47, 11, 60];
+                randoNumbero[4] = 44;   // changes the '90' value to 44
+                let nuke = randoNumbero[4]; console.log(nuke)   // elements in an array can be changed
+                var arrayArrayYeah = [[1, 2, 3, 4, 5, 6], [7, 8, 9, 10], [[11, 12, 13, 14], 15, 16]];
+                var oops = arrayArrayYeah[2] [0] [2];  console.log(oops)  // Targeting an element within multi-dimensional arrays
+
+                let myShit = ["cool", "damn", "crazy"]
+                myShit.push(["calm", "chill","noodles"]);
+                console.log(myShit); // the push function helps add elements or other arrays into the previous
+
+                let normalArray = [2, 4, 6, 8, 9];
+                var stolen = normalArray.pop(); // the pop function removes the last element in an array and adds it to another variable
+                console.log(stolen)  // this now equals the removed element
+                console.log(normalArray)  // this equals the array minus the removed element
