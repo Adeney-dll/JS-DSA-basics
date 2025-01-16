a77adfac495f7458e2cb6c2f9b1f1ead68ad74e4
Adene Daniel <philocity@proton.me>
2025-01-16 11:40:29 +0100

    Update JS-DSA.js

--- JS-DSA.js
+++ JS-DSA.js
@@ -9,9 +9,13 @@ row.push(["calm", "chill","noodles"]); // the push function helps add elements o
 let normalArray = [2, 4, 6, 8, 9];
 let stolen = normalArray.pop(); // the pop function removes the last element in an array and adds it to another variable
 console.log(stolen)  // this now equals the removed element
-console.log(normalArray)  // this equals the array minus the removed element 
+console.log(normalArray)  // this equals the array minus the removed element
+
+// "#" pyramid project 
 // Use of const variables //
+const character = "#";
 const name = "dlex";
 const count = 8;  // const variables can not be reassinged, they also can't be left empty or uninitialized 
 // for loops : for (iterator; condition; iteration){logic;}
-for (let i = 0; i < count; i = i + 1) {console.log(i)
+for (let i = 0; i < count; i = i + 1) {row.push(i)}; // writes out the digits 0 to 7, each on a line, command within curly bracket is currently to push it to the row array. //
+let result = ""; //so as to visually track progress 
