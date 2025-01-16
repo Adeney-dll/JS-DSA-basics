48781591762f0bf685f086d6ea8873ef1bc44006
Adene Daniel <philocity@proton.me>
2025-01-16 11:26:41 +0100

    Update JS-DSA.js

--- JS-DSA.js
+++ JS-DSA.js
@@ -5,3 +5,13 @@ character = "Him within";  // Reassigning a variable //
 let theNewOne  // camelcase writing for js //
 let row = ["john", "Joe", "Jake", "Jonathan"];  // arrays //
 row[1] = "Jane";  // changing values in an array //
+row.push(["calm", "chill","noodles"]); // the push function helps add elements or other arrays into the previous
+let normalArray = [2, 4, 6, 8, 9];
+let stolen = normalArray.pop(); // the pop function removes the last element in an array and adds it to another variable
+console.log(stolen)  // this now equals the removed element
+console.log(normalArray)  // this equals the array minus the removed element 
+// Use of const variables //
+const name = "dlex";
+const count = 8;  // const variables can not be reassinged, they also can't be left empty or uninitialized 
+// for loops : for (iterator; condition; iteration){logic;}
+for (let i = 0; i < count; i = i + 1) {console.log(i)
